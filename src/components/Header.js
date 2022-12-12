950a3586cc89d63f703b724680ae74877c19f850
Chanfuc <115201490+Chanfuc812@users.noreply.github.com>
2022-12-12 08:24:53 +0700

    update full components

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,7 +27,7 @@ const Header = () => {
         <Tabs textColor="inherit"
           value={value} onChange={(e, val) =>
             setValue(val)}>
-         
+          <Tab LinkComponent={Link} to="/blogs" label="All Blogs" />
           <Tab LinkComponent={Link} to="/myBlogs" label="My Blogs" />
         </Tabs>
       </Box>}
