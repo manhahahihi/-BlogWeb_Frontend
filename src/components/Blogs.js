5b5819ceb719a3b73908dba6415e210b209c154f
cuongviet130401 <cuongviet130401@gmail.com>
2022-12-13 12:08:44 +0700

    up Blogs

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import BlogCard from "./BlogCard";
+
 
 const Blogs = () => {
+  const [blogs, setBlogs] = useState();
+  const sendRequest = async () => {
+    const res = await axios
+      .get("http://localhost:5000/api/blog")
+      .catch((err) => console.log(err));
+    const data = await res.data;
+    return data;
+  };
+  useEffect(() => {
+    sendRequest().then((data) => setBlogs(data.blogs));
+  }, []);
+  console.log(blogs);
   return (
     <div>
-      Blogs
+      {blogs &&
+        blogs.map((blog, index) => (
+          <BlogCard
+            id={blog._id}
+            isUser={localStorage.getItem("userId") === blog.user._id}
+            title={blog.title}
+            description={blog.description}
+            imageURL={blog.image}
+            userName={blog.user.userName}
+          />
+        ))}
     </div>
-  )
-}
+  );
+};
 
-export default Blogs
+export default Blogs;
