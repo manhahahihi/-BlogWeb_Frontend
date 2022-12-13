e6e8afad0560cff01ab049337d63903deb617b4e
unknown <91258792+salaghati@users.noreply.github.com>
2022-12-13 20:13:50 +0700

    Update BlogCard.js

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -4,15 +4,7 @@ import React from 'react'
 const BlogCard = ({title,description,imageURL,userName}) => {
     return (
         <div>
-            <Card sx={{
-          width: "40%",
-          margin: "auto",
-          mt: 2,
-          padding: 2,
-          boxShadow: "5px 5px 10px #ccc",
-          ":hover": {
-            boxShadow: "10px 10px 20px #ccc",
-          },
+           
         }}>
                 <CardHeader
                     avatar={
